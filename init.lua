47b180ca730541d0b13bcbacd053b154385b5c25
bilet-13 <r10922114@csie.ntu.edu.tw>
2025-03-22 21:35:14 +0800

    Resolve merge conflicts in init.lua

--- init.lua
+++ init.lua
@@ -15,6 +15,10 @@ vim.g.mapleader = " "               -- Set leader key to Space
 vim.keymap.set("n", "<leader>w", ":w<CR>", { silent = true }) -- Save file
 vim.keymap.set("n", "<leader>q", ":q<CR>", { silent = true }) -- Quit file
 vim.keymap.set("n", "gr", vim.lsp.buf.references, { silent = true })
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true }) -- exit
+-- tab navigation shortcuts
+vim.keymap.set('n', '<leader>j', ':tabprevious<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>k', ':tabnext<CR>', { noremap = true, silent = true })
 
 -- Install lazy.nvim plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -29,19 +33,34 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Plugin Setup
 require("lazy").setup({
+	{ "folke/tokyonight.nvim", lazy = false, priority = 1000 },
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }, -- Syntax highlighting
   { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } }, -- Fuzzy finder
+   { "nvim-tree/nvim-tree.lua", -- File tree explorer
+    dependencies = {
+      "nvim-tree/nvim-web-devicons", -- Optional icons
+    },
+    config = function()
+      require("nvim-tree").setup({
+        view = { width = 30, side = "left" },
+        renderer = { group_empty = true },
+        filters = { dotfiles = false },
+      })
+    end
+  },
   { "nvim-lualine/lualine.nvim" }, -- Status line
   { "tpope/vim-commentary" }, -- Comment toggle
   { "neovim/nvim-lspconfig" }, -- LSP support
   { "hrsh7th/nvim-cmp", dependencies = { "hrsh7th/cmp-nvim-lsp" } }, -- Autocomplete
   { "L3MON4D3/LuaSnip" }, -- Snippet support
   { "stevearc/oil.nvim" }, -- file explorer
-  { "catppuccin/nvim", as = "catppuccin" },
+  -- { "catppuccin/nvim", as = "catppuccin" },
 })
 
 -- theme
-vim.cmd.colorscheme("catppuccin")
+vim.g.tokyonight_style = "night"
+vim.cmd.colorscheme("tokyonight")
+-- vim.cmd.colorscheme("catppuccin")
 
 -- Treesitter Configuration
 require("nvim-treesitter.configs").setup {
@@ -77,6 +96,7 @@ vim.keymap.set("n", "<leader>e", ":Oil<CR>", { silent = true }) -- Open file man
 vim.keymap.set("n", "<leader>f", ":Telescope find_files<CR>", { silent = true })
 vim.keymap.set("n", "<leader>g", ":Telescope live_grep<CR>", { silent = true })
 vim.keymap.set("n", "<leader>r", ":Telescope lsp_references<CR>", { silent = true })
+vim.keymap.set('n', '<leader>o', ':NvimTreeToggle<CR>', { silent = true })
 
 -- LSP Configuration
 local lspconfig = require("lspconfig")
@@ -93,6 +113,20 @@ cmp.setup({
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
   },
   sources = { { name = "nvim_lsp" } },
+})
+
+-- NvimTree keymap: open file in new tab with 't'
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "NvimTree",
+  callback = function()
+    vim.keymap.set("n", "t", function()
+     local api = require("nvim-tree.api")
+     local node = api.tree.get_node_under_cursor()
+      if node and node.link_to or node.nodes == nil then
+        vim.cmd("tabnew " .. node.absolute_path)
+      end
+    end, { buffer = true, silent = true })
+  end,
 })
 
 -- setting file name and parent as the warp tab name 
